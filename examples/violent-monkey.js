de279e3854dfad63a3bb1c19d1229863086e6c52
Jan Hermes <jan.hermes@student.kit.edu>
2020-04-19 09:22:01 +0200

    set cdn to 0.0.5

--- examples/violent-monkey.js
+++ examples/violent-monkey.js
@@ -5,7 +5,7 @@
 // @include      https://gingkoapp.com/*
 // @run-at       document-start
 // @grant        none
-// @require  https://cdn.jsdelivr.net/gh/gingko-extensions/html-filter@0.0.2/src/html-filter.js
+// @require  https://cdn.jsdelivr.net/gh/gingko-extensions/html-filter@0.0.5/src/html-filter.js
 // ==/UserScript==
 
 /*
